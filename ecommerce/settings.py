139cd3d06411f1448c693dd9258af492c472a243
Aquon Bovell <aquon_bovell@outlook.com>
2024-06-19 14:38:30 -0400

    changed database

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -85,14 +85,14 @@ WSGI_APPLICATION = "ecommerce.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-        # "ENGINE": "django.db.backends.postgresql",
-        # "NAME":"railway",
-        # "USER":"postgres",
-        # "PASSWORD": os.environ.get("DB_PASSWORD"),
-        # "HOST":"monorail.proxy.rlwy.net",
-        # "PORT":"47314",
+        # "ENGINE": "django.db.backends.sqlite3",
+        # "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME":"railway",
+        "USER":"postgres",
+        "PASSWORD": os.environ("DB_PASSWORD_ECON"),
+        "HOST":"monorail.proxy.rlwy.net",
+        "PORT":"47314",
     }
 }
 
